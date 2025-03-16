496da56617d983bc3b219541dc8cfeede48de10b
Mohit Kumar <mohitkumar753119@gmail.com>
2025-03-16 14:47:42 +0530

    Update Patternt_string.py

--- simple_project/Patternt_string.py
+++ simple_project/Patternt_string.py
@@ -1,6 +1,10 @@
 def print_pattern():
     string = input("Enter a string: ")
-    for i in range(1, len(string) + 1):
-        print(string[:i])
+    if string:
+        for i in range(1, len(string) + 1):
+            print(string[:i])
+    else:
+        print("kaya...\nKaya bak rahe ho madherchod...????")
 
 print_pattern()
+

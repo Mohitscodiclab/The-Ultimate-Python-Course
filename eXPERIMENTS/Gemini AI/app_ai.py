9f65263163ac9b36862ed682227ad74267d097ae
Mohit Kumar <mohitkumar753119@gmail.com>
2025-04-26 04:24:32 +0530

    Initial commit

---
+++ eXPERIMENTS/Gemini AI/app_ai.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+
+
+
+# API_KEY = "Your_API_KEY"  # Replace with your actual API key
+API_KEY = "AIzaSyCeOcqAsfak54JodEd5pKU86kV0tzVeKWU"  # Replace with your actual API key
+genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-pro-latest")  
+chat = model.start_chat()
+
+
+print("Chat with Gemini-1.5-pro-latest Type 'exit' to end the conversation.")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+        break
+    response = chat.send_message(user_input)
+    print("Gemini : ", response.text)
